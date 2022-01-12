be2a43643f60e4bf307df5adc5da60c8dc4bc21a
LAKSHANYAJINDAL <97080663+LAKSHANYAJINDAL@users.noreply.github.com>
2022-01-12 19:09:55 +0530

    Add files via upload

---
+++ PYTHON ASSIGNMENT 1 OUTPUT.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\DELL\Desktop\assignment 1\PYTHON ASSIGNMENT 1 INPUT.py ===
+PYTHON ASSIGNMENT 1
+Q1
+Enter no.1-3
+Enter no.2-6
+Enter no.3-9
+Average is 6.0
+Q2
+Enter gross income-$100000
+Enter no. of dependents-2
+Tax = $16800.0
+Q3
+Enter SID-21104054
+Enter Name-LAKSHANYA
+Enter Gender(F,M,U-FOR UNKNOWN)-F
+Enter Course name-EE(COMPUTING)
+Enter CGPA-9
+Student [21104054, 'LAKSHANYA', 'F', 'EE(COMPUTING)', 9.0]
+Q4
+Enter marks of student 1-10
+Enter marks of student 2-20
+Enter marks of student 3-30
+Enter marks of student 4-40
+Enter marks of student 5-50
+Sorted list of marks [10, 20, 30, 40, 50]
+Q5
+color ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Part A-List after removing 4th element
+ ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part B-Replacing black and pink with purple
+ ['Red', 'Green', 'White', 'Purple', 'Yellow']
